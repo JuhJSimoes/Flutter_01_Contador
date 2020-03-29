7f8768d63e149d35d1f17af0446dcdf8dea7cc2c
JuhJSimoes <juianajsimoes@gmail.com>
2020-03-29 17:22:16 -0300

    git@github.com:JuhJSimoes/Flutter_01_Contador.git

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+
+  final estiloTexto = new TextStyle(fontSize: 30)
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Título - Flutter_01'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Contando os clicks: ', style: estiloTexto),
+            Text('0', style: estiloTexto),
+          ],
+        )
+      ),
+    );
+  }
+}
